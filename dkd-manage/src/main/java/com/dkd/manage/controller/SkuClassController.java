8f6003d040703fd152268fa23960b5889d432f37
zetian <zetian.zhu@foxmail.com>
2025-01-22 20:11:07 +0800

    fix: v5;

--- dkd-manage/src/main/java/com/dkd/manage/controller/SkuClassController.java
+++ dkd-manage/src/main/java/com/dkd/manage/controller/SkuClassController.java
@@ -1,46 +1,42 @@
 package com.dkd.manage.controller;
 
-import java.util.List;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.dkd.common.annotation.Log;
 import com.dkd.common.core.controller.BaseController;
 import com.dkd.common.core.domain.AjaxResult;
+import com.dkd.common.core.page.TableDataInfo;
 import com.dkd.common.enums.BusinessType;
+import com.dkd.common.utils.poi.ExcelUtil;
 import com.dkd.manage.domain.SkuClass;
 import com.dkd.manage.service.ISkuClassService;
-import com.dkd.common.utils.poi.ExcelUtil;
-import com.dkd.common.core.page.TableDataInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * 商品类型Controller
- * 
+ *
  * @author zetian
  * @date 2024-12-15
  */
 @RestController
 @RequestMapping("/manage/skuClass")
-public class SkuClassController extends BaseController
-{
+public class SkuClassController extends BaseController {
+    private final ISkuClassService skuClassService;
+
     @Autowired
-    private ISkuClassService skuClassService;
+    public SkuClassController(ISkuClassService skuClassService) {
+        this.skuClassService = skuClassService;
+    }
 
     /**
      * 查询商品类型列表
      */
     @PreAuthorize("@ss.hasPermi('manage:skuClass:list')")
     @GetMapping("/list")
-    public TableDataInfo list(SkuClass skuClass)
-    {
+    public TableDataInfo list(SkuClass skuClass) {
         startPage();
         List<SkuClass> list = skuClassService.selectSkuClassList(skuClass);
         return getDataTable(list);
@@ -52,8 +48,7 @@ public class SkuClassController extends BaseController
     @PreAuthorize("@ss.hasPermi('manage:skuClass:export')")
     @Log(title = "商品类型", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
-    public void export(HttpServletResponse response, SkuClass skuClass)
-    {
+    public void export(HttpServletResponse response, SkuClass skuClass) {
         List<SkuClass> list = skuClassService.selectSkuClassList(skuClass);
         ExcelUtil<SkuClass> util = new ExcelUtil<SkuClass>(SkuClass.class);
         util.exportExcel(response, list, "商品类型数据");
@@ -64,8 +59,7 @@ public class SkuClassController extends BaseController
      */
     @PreAuthorize("@ss.hasPermi('manage:skuClass:query')")
     @GetMapping(value = "/{classId}")
-    public AjaxResult getInfo(@PathVariable("classId") Long classId)
-    {
+    public AjaxResult getInfo(@PathVariable("classId") Long classId) {
         return success(skuClassService.selectSkuClassByClassId(classId));
     }
 
@@ -75,8 +69,7 @@ public class SkuClassController extends BaseController
     @PreAuthorize("@ss.hasPermi('manage:skuClass:add')")
     @Log(title = "商品类型", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody SkuClass skuClass)
-    {
+    public AjaxResult add(@RequestBody SkuClass skuClass) {
         return toAjax(skuClassService.insertSkuClass(skuClass));
     }
 
@@ -86,8 +79,7 @@ public class SkuClassController extends BaseController
     @PreAuthorize("@ss.hasPermi('manage:skuClass:edit')")
     @Log(title = "商品类型", businessType = BusinessType.UPDATE)
     @PutMapping
-    public AjaxResult edit(@RequestBody SkuClass skuClass)
-    {
+    public AjaxResult edit(@RequestBody SkuClass skuClass) {
         return toAjax(skuClassService.updateSkuClass(skuClass));
     }
 
@@ -96,9 +88,8 @@ public class SkuClassController extends BaseController
      */
     @PreAuthorize("@ss.hasPermi('manage:skuClass:remove')")
     @Log(title = "商品类型", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{classIds}")
-    public AjaxResult remove(@PathVariable Long[] classIds)
-    {
+    @DeleteMapping("/{classIds}")
+    public AjaxResult remove(@PathVariable Long[] classIds) {
         return toAjax(skuClassService.deleteSkuClassByClassIds(classIds));
     }
 }
